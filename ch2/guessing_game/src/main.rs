b49eac6264eb88dd522cb73a55312f6e9a4988de
KIm, Hyeonseo <dodok8@gmail.com>
2020-08-12 01:52:48 +0900

    Add cmp Ordering ch2.4

--- ch2/guessing_game/src/main.rs
+++ ch2/guessing_game/src/main.rs
@@ -1,10 +1,11 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("숫자를 맞혀봅시다!");
 
-    let secret_number = rand::thread_rng().gen_range(1,101);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("사용자가 맞혀야 할 숫자: {}", secret_number);
 
@@ -16,4 +17,10 @@ fn main() {
         .read_line(&mut guess)
         .expect("입력한 값을 읽지 못했습니다");
     println!("입력한 값: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("입력한 숫자가 작습니다!"),
+        Ordering::Greater => println!("입력한 숫자가 큽니다!"),
+        Ordering::Equal => println!("정답!"),
+    };
 }
