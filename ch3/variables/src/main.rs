8b3a53271bdb2a1557e9a50aa4d41616f381a720
KIm, Hyeonseo <dodok8@gmail.com>
2020-08-12 06:44:37 +0900

    Add shadowed variables exmpale ch 3.1.2

--- ch3/variables/src/main.rs
+++ ch3/variables/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
-    let mut x =5;
-    println!("x의 값: {}", x);
-    x = 6;
-    println!("x의 값: {}", x);
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+    println!("x의 값 {}", x);
 }
