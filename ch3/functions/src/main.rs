98bb9413fa93415af4509208d62ce88315b85fc3
KIm, Hyeonseo <dodok8@gmail.com>
2020-08-13 01:55:03 +0900

    Add an example 3.3.2

--- ch3/functions/src/main.rs
+++ ch3/functions/src/main.rs
@@ -1,8 +1,10 @@
 fn main() {
-    another_function(5, 6);
-}
+    let x = 5;
 
-fn another_function(x: i32, y: i32) {
-    println!("x의 값: {}", x);
-    println!("y의 값: {}", y);
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("y의 값 {}", y);
 }
