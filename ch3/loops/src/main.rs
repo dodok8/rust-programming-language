dc2c08a7f196f4ee6dd9fe3498dc5b3f2d68ad2f
KIm, Hyeonseo <dodok8@gmail.com>
2020-08-16 02:19:28 +0900

    Add example ch3.5.2 (2)

--- ch3/loops/src/main.rs
+++ ch3/loops/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+            // 뭔가 return을 써도 될 것 같지만 안 된다.
+        }
+    };
+
+    println!("The result is {}", result);
 }
