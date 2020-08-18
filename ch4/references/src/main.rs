c4aba38bfa5eed2efc12ce22e6c7a4fc98e1ab48
KIm, Hyeonseo <dodok8@gmail.com>
2020-08-19 05:40:37 +0900

    Add example about references ch4.2

--- ch4/references/src/main.rs
+++ ch4/references/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("'{}'의 길이는 {}입니다.", s1, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    return s.len();
 }
