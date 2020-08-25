a27c532f3bfa64f1ad6abf2a37843e439111636c
KIm, Hyeonseo <dodok8@gmail.com>
2020-08-25 23:35:41 +0900

    Add Exmaple tuple structs ch.5.2.1

--- ch5/rectangles/src/main.rs
+++ ch5/rectangles/src/main.rs
@@ -1,10 +1,18 @@
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    // let width1 = 30;
+    // let height1 = 50;
 
-    println!("사각형의 면적: {} 제곱픽셀", area(width1, height1));
+    // println!("사각형의 면적: {} 제곱 픽셀", area(width1, height1));
+
+    let rect1 = (30, 50); // tuple structs 활용
+
+    println!("사각형의 면적: {} 제곱 픽셀", area(rect1));
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    return width * height;
+// fn area(width: u32, height: u32) -> u32 {
+//     return width * height;
+// }
+
+fn area(dimensions: (u32, u32)) -> u32 {
+    return dimensions.0 * dimensions.1;
 }
